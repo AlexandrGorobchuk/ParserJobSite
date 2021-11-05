0dbf7fda4a8c5c952f5adeb41ddbb6b766aa0fd8
User <User@DESKTOP-05B6G7U>
2021-11-05 18:35:32 +0200

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using AngleSharp.Dom;
+using AngleSharp.Html.Parser;
+using System;
+using System.Net.Http;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            textBoxt();
+        }
+        private void textBoxt()
+        {
+            string x = "Горобчук Александр";
+            string y = "Горобчук Виктор";
+            textBoxName.Text = x + Environment.NewLine + y;
+        }
+
+        private async void ParseRabotaUa(string value)
+        {
+            IHtmlParser htmlParser = new HtmlParser();
+            string path = $"https://rabota.ua/candidates/{value}/вся_украина?pg=3&period=" + "\"All\"&searchType=\"everywhere\"";
+            using (HttpClient httpClient = new HttpClient())
+            {
+                IDocument html = await htmlParser.ParseDocumentAsync(await httpClient.GetStringAsync(path));
+                var collect = html.QuerySelectorAll(@"section.cv-card");
+                //var ee = collect[0].Text().Split('\n');
+
+                Console.WriteLine(html.TextContent);
+            };
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ParseRabotaUa("Аврора");
+        }
+    }
+}
